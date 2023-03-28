f258887790bfca93992b067ed102bef653f9f306
Vijay Katoch <vijay.katoch@de.abb.com>
2023-03-28 15:33:05 +0200

    updated to include CanOpen

--- flightControl/src/app/main.c
+++ flightControl/src/app/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "CO_app_STM32.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
